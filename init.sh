76f06d5fb1464910321c8d7a5bff4b060f8f8d48
Vijay Kumar <vijayarya326@gmail.com>
2019-11-30 20:56:21 +0530

    ˚addded to Zone test

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# zone-example" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/aryavijay/zone-example.git
+git push -u origin master
